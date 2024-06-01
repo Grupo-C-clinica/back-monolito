e84ee43136442df035e7d0f5b8486225c62a1d72
gabyportu <gabriela.portugal@ucb.edu.bo>
2024-06-01 10:55:38 -0400

    Inicio

---
+++ src/main/java/com/example/back_monolito/BackMonolitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_monolito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackMonolitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/back_monolito/BackMonolitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_monolito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMonolitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
