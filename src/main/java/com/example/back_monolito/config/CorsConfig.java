b2a7be72965e18a31788a2176ba579e26643189f
615Alberto615 <a.encinas115@gmail.com>
2024-06-02 21:16:00 -0400

    arreglos y deploy

---
+++ src/main/java/com/example/back_monolito/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.back_monolito.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite CORS en todas las rutas
+                .allowedOrigins("http://localhost:5173")  // Permite solicitudes desde este origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos HTTP permitidos
+                .allowedHeaders("*")  // Permite todos los headers
+                .allowCredentials(true);  // Permite cookies y autenticación básica
+    }
+
+}
