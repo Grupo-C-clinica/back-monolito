504b24e867ae7c6655b31e0321d146beac4e178c
lim2002 <limbert.quispe@ucb.edu.bo>
2024-06-14 17:38:04 -0400

    Arreglos metodo endpoint de suscribirse

--- src/main/java/com/example/back_monolito/Api/SuscripcionApi.java
+++ src/main/java/com/example/back_monolito/Api/SuscripcionApi.java
@@ -3,6 +3,7 @@ package com.example.back_monolito.Api;
 import com.example.back_monolito.Bl.SuscripcionBl;
 import com.example.back_monolito.Dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,15 +15,17 @@ public class SuscripcionApi {
     private SuscripcionBl suscripcionBl;
 
     //Suscribirse
-    @GetMapping (path = "/{email}")
-    public ResponseEntity<ResponseDto<String>> suscribirse(@PathVariable String email){
-        try{
+    @GetMapping(path = "/{email}")
+    public ResponseEntity<ResponseDto<String>> suscribirse(@PathVariable String email) {
+        try {
             suscripcionBl.suscribirse(email);
             return ResponseEntity.ok(new ResponseDto<>(200, email, "Gracias por suscribirte"));
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.ok(new ResponseDto<>(500, null, "Error al suscribirte"));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto<>(500, null, "Error al suscribirse email incorrecto"));
         }
     }
+
 
 }
